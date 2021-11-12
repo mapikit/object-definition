20e215f23a68b326262e08ed572206bc3a46b401
Fábio Meneses Junior <flgmjr@gmail.com>
2021-11-11 23:04:14 -0300

    move index outside of  src

---
+++ index.ts
@@ -0,0 +1,14 @@
+import { AcceptedTypes, ObjectDefinition } from "./src/object-definition-type";
+import { isObjectDefinition } from "./src/functions/is-object-definition";
+import { validateObject } from "./src/functions/validate-object";
+
+export {
+  AcceptedTypes,
+  ObjectDefinition,
+}
+
+export default {
+  isObjectDefinition,
+  validateObject,
+}
+
