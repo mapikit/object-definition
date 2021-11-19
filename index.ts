235850e92abee7889d46a5b2f215797292b67d07
Fábio Meneses Junior <flgmjr@gmail.com>
2021-11-19 10:06:26 -0300

    export getAllTypesInDefinition

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import { AcceptedTypes, ObjectDefinition } from "./src/object-definition-type";
 import { isObjectDefinition } from "./src/functions/is-object-definition";
 import { validateObject } from "./src/functions/validate-object";
+import { getAllTypesInDefinition } from "./src/get-all-types-in-definition";
 
 export {
   isObjectDefinition,
+  getAllTypesInDefinition,
   validateObject,
   AcceptedTypes,
   ObjectDefinition,
