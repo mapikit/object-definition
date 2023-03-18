7b7c52144356c799404725cfaf497205f9599276
Fábio Meneses Junior <flgmjr@gmail.com>
2023-03-18 02:10:34 -0300

    migrate to ESModule and update dependencies

--- test/data/stub-definitions.ts
+++ test/data/stub-definitions.ts
@@ -2,34 +2,34 @@ import { ObjectDefinition } from "../../src/object-definition-type";
 
 export const stubDefinition1 : ObjectDefinition = {
   "aProp": {
-      "type": "object",
-      "required": true,
-      "subtype": {
-          "inner": {
-              "type": "array",
-              "required": false,
-              "subtype": {
-                          "4321": {
-                              "type": "number",
-                              "required": false
-                          },
-                          "test": {
-                              "type": "object",
-                              "required": false,
-                              "subtype": {
-                                  "This is a deep test bro": {
-                                      "type": "boolean",
-                                      "required": false
-                                  }
-                              }
-                          },
-                          "again": {
-                              "type": "date",
-                              "required": false
-                          }
-                      }
+    "type": "object",
+    "required": true,
+    "subtype": {
+      "inner": {
+        "type": "array",
+        "required": false,
+        "subtype": {
+          "4321": {
+            "type": "number",
+            "required": false
+          },
+          "test": {
+            "type": "object",
+            "required": false,
+            "subtype": {
+              "This is a deep test bro": {
+                "type": "boolean",
+                "required": false
+              }
+            }
+          },
+          "again": {
+            "type": "date",
+            "required": false
           }
+        }
       }
+    }
   }
 };
 
